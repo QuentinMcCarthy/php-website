124ccd42ff68c26b243a97a9fe2305834322a27b
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-09 08:47:46 +1300

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>PHP Website</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/master.css">
+	</head>
+	<body>
+		<h1>Basic PHP Website</h1>
+
+		<script src="js/script.js"></script>
+	</body>
+</html>
