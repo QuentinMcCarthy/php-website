3deb92b485bbe68cdcce0e4208e883cf3b86fda0
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-09 10:50:10 +1300

    Contact form created

--- contact.php
+++ contact.php
@@ -5,11 +5,28 @@
 ?>
 
 			<main role="main" class="inner cover">
-				<h1 class="cover-heading">Contact page, woop</h1>
-				<p class="lead">Contact whoever made this template, idk. Looks like it was made by this @mdo guy.</p>
-				<p class="lead">
-					<a href="#" class="btn btn-lg btn-secondary">Contact him.</a>
-				</p>
+				<h1 class="cover-heading">Contact</h1>
+				<p class="lead">Send us an Email</p>
+
+				<form method="POST" action="contact.php" enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" name="name" class="form-control" placeholder="Enter Name" value="">
+					</div>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" name="email" class="form-control" placeholder="Enter Email" value="" required>
+					</div>
+					<div class="form-group">
+						<label for="message">Message</label>
+						<textarea name="message" rows="3" class="form-control"></textarea>
+					</div>
+					<div class="form-group">
+						<input type="checkbox" name="subscribe" class="form-check-input" id="subscribe">
+						<label class="form-check-label" for="subscribe">Subscribe to Newsletter</label>
+					</div>
+					<button type="submit" class="btn btn-outline-light btn-block">Submit</button>
+				</form>
 			</main>
 
 <?php require "includes/footer.php"; ?>
