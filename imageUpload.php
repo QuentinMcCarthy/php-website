e24bb22b692b459fa4e8268826143bf4034062e5
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-10 10:15:13 +1300

    Image uploading added

---
+++ imageUpload.php
@@ -0,0 +1,69 @@
+<?php
+	// phpinfo();
+	// die();
+
+	$errors = array();
+	if(isset($_FILES["image"])){
+		$fileSize = $_FILES["image"]["size"];
+		$fileTmp = $_FILES["image"]["tmp_name"];
+		$fileType = $_FILES["image"]["type"];
+
+		// var_dump($fileSize);
+		// var_dump($fileTmp);
+		// var_dump($fileType);
+
+		// If the file is over 5mb
+		if($fileSize > 5000000){
+			array_push($errors, "The file is too large, must be under 5MB");
+		}
+
+		$validExtensions = array(
+			"jpeg",
+			"jpg",
+			"png"
+		);
+		$fileNameArray = explode(".", $_FILES["image"]["name"]);
+		$fileExt = strtolower(end($fileNameArray));
+
+		if(!in_array($fileExt, $validExtensions)){
+			array_push($errors, "File type not allowed, can only be a jpg or png");
+		}
+
+		$destination = "img/uploads";
+		if(!is_dir($destination)){
+			mkdir($destination."/", 0777, true);
+		}
+
+		$newFileName = uniqid().".".$fileExt;
+
+		move_uploaded_file($fileTmp, $destination."/".$newFileName);
+	}
+
+	$page = "imageUpload";
+	require "includes/head.php";
+?>
+
+<main role="main" class="inner cover">
+	<h1 class="cover-heading">Image Upload Page</h1>
+	<p class="lead">Upload an image to our server</p>
+
+	<?php if($_FILES && !empty($errors)): ?>
+		<div class="alert alert-danger" role="alert">
+			<ul>
+				<?php foreach($errors as $error): ?>
+					<li><?= $error; ?></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	<?php endif; ?>
+
+	<form  action="imageUpload.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="image">Upload an Image</label>
+            <input type="file" name="image" class="form-control-file">
+        </div>
+        <button type="submit" class="btn btn-outline-light btn-block">Submit</button>
+	</form>
+</main>
+
+<?php require "includes/footer.php" ?>
